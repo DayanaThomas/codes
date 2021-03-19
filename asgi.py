cbf2dec3af9aa01c77d7ce8498fe0a795aad64fa
DayanaThomas <80392152+DayanaThomas@users.noreply.github.com>
2021-03-19 14:44:49 +0530

    placeguess

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for imageupload project.
+ASGI config for placeguess project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageupload.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placeguess.settings')
 
 application = get_asgi_application()
